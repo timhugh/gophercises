7304affb6af0ea343df304ffdb2640ae3e812ccf
Tim Heuett <info@timheuett.com>
2018-04-01 19:16:26 -0700

    adds time limit (part two) (#2)

--- 01-quiz/main.go
+++ 01-quiz/main.go
@@ -1,64 +1,87 @@
 package main
 
 import (
-	"bufio"
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"os"
-	"strings"
+	"time"
 )
 
-type Question struct {
+type quiz struct {
+	Questions      []question
+	TimeLimit      time.Duration
+	CorrectAnswers int
+}
+
+type question struct {
 	Prompt string
 	Answer string
 }
 
 func main() {
-	var problemFile string
-	flag.StringVar(&problemFile, "file", "problems.csv", "CSV file containing the quiz questions")
+	csvFile := flag.String("file", "problems.csv", "CSV file containing the quiz questions")
+	timeLimit := flag.Int("limit", 30, "Time limit for quiz completion")
+	// shuffle := flag.Bool("shuffle", false, "Shuffle problem ordering")
 	flag.Parse()
 
-	f, err := os.Open(problemFile)
-	check(err)
+	f, err := os.Open(*csvFile)
+	if err != nil {
+		panic(fmt.Sprintf("Unable to open CSV file: %s", err))
+	}
 	defer f.Close()
 
-	s := bufio.NewScanner(f)
-
-	var questions []Question
-	for s.Scan() {
-		row := string(s.Bytes())
-		question := parseQuestionFromCSV(row)
-		questions = append(questions, question)
-	}
-	check(s.Err())
-
-	runQuiz(questions)
-}
-
-func parseQuestionFromCSV(row string) Question {
-	parts := strings.Split(row, ",")
-	return Question{
-		Prompt: parts[0],
-		Answer: parts[1],
-	}
-}
-
-func check(err error) {
+	questions, err := parseFile(f)
 	if err != nil {
-		panic(err)
+		panic(fmt.Sprintf("Unable to parse CSV file: %s", err))
 	}
+
+	quiz := &quiz{
+		Questions: questions,
+		TimeLimit: time.Duration(*timeLimit) * time.Second,
+	}
+	quiz.Run()
 }
 
-func runQuiz(questions []Question) {
-	rightAnswers := 0
-	reader := bufio.NewReader(os.Stdin)
-	for i, question := range questions {
-		fmt.Printf("#%d: %s\n", i+1, question.Prompt)
-		answer, err := reader.ReadString('\n')
-		check(err)
-		if strings.TrimSuffix(answer, "\n") == question.Answer {
-			rightAnswers++
+func parseFile(f *os.File) ([]question, error) {
+	r := csv.NewReader(f)
+	lines, err := r.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	questions := make([]question, len(lines))
+	for i, line := range lines {
+		questions[i] = question{
+			Prompt: line[0],
+			Answer: line[1],
 		}
 	}
-	fmt.Printf("You scored %d out of %d.\n", rightAnswers, len(questions))
+	return questions, nil
+}
+
+func (q *quiz) Run() {
+	timer := time.NewTimer(q.TimeLimit)
+
+loop:
+	for i, question := range q.Questions {
+		fmt.Printf("#%d: %s\n", i+1, question.Prompt)
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			break loop
+		case answer := <-answerCh:
+			if answer == question.Answer {
+				q.CorrectAnswers++
+			}
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d.\n", q.CorrectAnswers, len(q.Questions))
 }
